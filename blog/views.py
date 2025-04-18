722ab7678e43bbca1487929b3bb46769b82c8c3a
anacarolbs <souzaanacarolb@gmail.com>
2025-04-18 02:03:22 -0300

    Adicionar views e templates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+#import the Post and the Comment models
+from blog.models import Post, Comment
 
-# Create your views here.
+#blog_index() will display a list of all your posts
+def blog_index(request):
+    #obtain a Queryset containing all the posts in the database. A Queryset is a collection of all the objects in the database that match the query.
+    posts = Post.objects.all().order_by("-created_on") # (-): get the recently created posts first
+    context = {
+        "posts": posts,
+    }
+    #define a context dictionary and render a template named index.html
+    return render(request, "blog/index.html", context)
+
+
+#blog_detail() will display the full post. Later, this view will also show existing comments and a form to allow users to create new comments.
+def blog_category(request, category):
+    # Django Queryset filter. The argument of the filter tells Django what conditions need to be true to retrieve an object. 
+    # In this case, you only want posts whose categories contain the category with the name corresponding to what’s given in the argument of the view function.
+    posts = Post.objects.filter(
+        categories__name__contains=category
+    ).order_by("-created_on")
+    context = {
+        "category": category,
+        "posts": posts,
+    }
+    return render(request, "blog/category.html", context)
+
+
+#blog_category() will be similar to blog_index, but the posts shown will only be of a specific category that the user chooses.
+def blog_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    comments = Comment.objects.filter(post=post)
+    context = {
+        "post": post,
+        "comments": comments,
+    }
+
+    return render(request, "blog/detail.html", context)
