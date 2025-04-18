814d5a822329981f235bf7038a5386783446724c
anacarolbs <souzaanacarolb@gmail.com>
2025-04-18 18:51:56 -0300

    Alteracoes de layout

--- personal_blog/settings.py
+++ personal_blog/settings.py
@@ -70,6 +70,7 @@ TEMPLATES = [
     },
 ]
 
+
 WSGI_APPLICATION = 'personal_blog.wsgi.application'
 
 
@@ -118,7 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "blog/static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
