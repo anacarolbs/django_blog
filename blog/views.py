6f8914562bbe0963225ad5f8d6c8b44c830c5fca
anacarolbs <souzaanacarolb@gmail.com>
2025-04-18 02:37:47 -0300

    Formulários para comentários do usuário

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 #import the Post and the Comment models
 from blog.models import Post, Comment
+from django.http import HttpResponseRedirect
+from blog.forms import CommentForm
 
 #blog_index() will display a list of all your posts
 def blog_index(request):
@@ -30,10 +32,22 @@ def blog_category(request, category):
 #blog_category() will be similar to blog_index, but the posts shown will only be of a specific category that the user chooses.
 def blog_detail(request, pk):
     post = Post.objects.get(pk=pk)
+    form = CommentForm()
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = Comment(
+                author=form.cleaned_data["author"],
+                body=form.cleaned_data["body"],
+                post=post,
+            )
+            comment.save()
+            return HttpResponseRedirect(request.path_info)
+
     comments = Comment.objects.filter(post=post)
     context = {
         "post": post,
         "comments": comments,
+        "form": CommentForm(),
     }
-
     return render(request, "blog/detail.html", context)
